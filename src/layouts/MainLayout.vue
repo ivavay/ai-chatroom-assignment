55bbc8ae182160086d7e65ee4ec5b3b5a02f120d
Ivy Chen <ivy0925chen@gmail.com>
2026-04-17 13:26:03 +0800

    remove: default html from starter files

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-drawer
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -26,7 +9,6 @@
 </template>
 
 <script setup>
-import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
